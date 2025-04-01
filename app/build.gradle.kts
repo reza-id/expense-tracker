e23960e213208a50f560ed2dde0cc71b13614043
Reza Agustiana <reza.agustiana@gmail.com>
2025-04-01 21:35:36 +0700

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.ksp)
 }
 
 android {
@@ -50,6 +52,46 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.material.icons)
+    implementation(libs.lifecycle.compose.viewmodel)
+    implementation(libs.navigation.compose)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // Koin for Dependency Injection
+    implementation(project.dependencies.platform(libs.koin.bom))
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    implementation(libs.koin.android.compose)
+    implementation(libs.koin.android.compose.navigation)
+
+    // Ktor for networking
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.auth)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    // Supabase
+    implementation(project.dependencies.platform(libs.supabase.bom))
+    implementation(libs.supabase.storage)
+    implementation(libs.supabase.postgrest)
+
+    // Date Time
+    implementation(libs.kotlinx.date.time)
+    // Kotlin Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    // Coil for image loading
+    implementation(libs.coil.compose)
+
+    // Camera
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
